b95c6f5721105e26c175873cafbc2b7f2a98b3af
kim keun tae <rmsxo2000@gmail.com>
2025-01-11 14:51:15 +0900

    서버 실행시 빈등록 오류 수정

--- cinema-adapter/build.gradle.kts
+++ cinema-adapter/build.gradle.kts
@@ -13,7 +13,9 @@ repositories {
 
 dependencies {
     implementation(project(":cinema-application")) // 애플리케이션 계층 의존성
+    implementation(project(":cinema-infrastructure")) // RepositoryPort 구현체를 찾지 못해서 추가
     implementation("org.springframework.boot:spring-boot-starter-web") // web
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa") //jpa 레포지토리 의존성 문제로 추가 (@EnableJpaRepositories)
 }
 
 tasks.test {
--- cinema-adapter/src/main/java/com/hanghae/adapter/AdapterApplication.java
+++ cinema-adapter/src/main/java/com/hanghae/adapter/AdapterApplication.java
@@ -2,9 +2,20 @@ package com.hanghae.adapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 // 다른 모듈의 빈을 찾지 못해 scanBasePackages 설정
-@SpringBootApplication(scanBasePackages = {"cinema-application", "cinema-adapter"})
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.hanghae.infrastructure.repository")
+@EntityScan(basePackages = "com.hanghae.infrastructure.entity")
+@ComponentScan(basePackages = {
+        "com.hanghae.domain",
+        "com.hanghae.application",
+        "com.hanghae.infrastructure",
+        "com.hanghae.adapter"
+})
 public class AdapterApplication {
     public static void main(String[] args) {
         //서버 실행시 어뎁터로 실행
