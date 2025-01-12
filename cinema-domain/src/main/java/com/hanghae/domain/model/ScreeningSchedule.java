7875ebfd11064bd4d7bb0cb520f3f77b0635b85b
kim keun tae <rmsxo2000@gmail.com>
2025-01-12 16:12:21 +0900

    상영일, 개봉일자 검증 로직 추가

--- cinema-domain/src/main/java/com/hanghae/domain/model/ScreeningSchedule.java
+++ cinema-domain/src/main/java/com/hanghae/domain/model/ScreeningSchedule.java
@@ -3,10 +3,10 @@ package com.hanghae.domain.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
 public class ScreeningSchedule {
     //일관된 상태 유지를 위해 @AllArgsConstructor 만 사용
     private final Long scheduleId;
@@ -17,4 +17,24 @@ public class ScreeningSchedule {
     private final LocalDateTime createdAt; // 작성일
     private final Long updatedBy; // 수정자 ID
     private final LocalDateTime updatedAt; //수정일
+
+    public ScreeningSchedule(Long scheduleId, Movie movie, Screen screen, LocalDateTime showStartDatetime, Long createdBy, LocalDateTime createdAt, Long updatedBy, LocalDateTime updatedAt) {
+        this.scheduleId = scheduleId;
+        this.movie = movie;
+        this.screen = screen;
+        this.showStartDatetime = showStartDatetime;
+        this.createdBy = createdBy;
+        this.createdAt = createdAt;
+        this.updatedBy = updatedBy;
+        this.updatedAt = updatedAt;
+
+        // 개봉일 검증 로직
+        validateReleaseDate(movie.getReleaseDate());
+    }
+
+    public void validateReleaseDate(LocalDate releaseDate) {
+        if (showStartDatetime.toLocalDate().isBefore(releaseDate)) {
+            throw new IllegalArgumentException("상영 날짜는 개봉일보다 이전일 수 없습니다.");
+        }
+    }
 }
